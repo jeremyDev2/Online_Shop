7bc160b4b1deabf51843820b48bea4b4ee471238
Daniel <danil.malaev3@gmail.com>
2026-02-26 17:47:59 +0200

    add: category models & product model

--- ecommerce_shop/shop/models.py
+++ ecommerce_shop/shop/models.py
@@ -1,2 +1,37 @@
 from django.db import models
 
+class Categoty(models.Model):
+    name = models.CharField(max_lenght=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering=["name"]
+        indexes=[models.Index(fields=["name"])]
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Product(models.Model):
+    
+    category = models.ForeignKey(Categoty,related_name='products',on_delete=CASCADE)
+    name = models.CharField(max_lenght=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank = True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField(max_digits=10, decimal_places = 2)
+    available = models.BooleanField()
+    created = models.DateTimeField(auto_now_add = True)
+    updated = models.DateTimeField(auto_now = True)
+
+
+    class Meta:
+        ordering = ['name']
+        indexes=[models.Index(fields=['id', 'slug']),
+                 models.Index(fields=['name']),
+                 models.Index(fields=['-created']),]
+
+    def __str__(self) -> str:
+        return str(self.name)
