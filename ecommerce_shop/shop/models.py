3dd0ad44b522f4abd55de37e97971eddc2805019
Daniel <danil.malaev3@gmail.com>
2026-02-26 18:06:53 +0200

    fix: typos

--- ecommerce_shop/shop/models.py
+++ ecommerce_shop/shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Categoty(models.Model):
-    name = models.CharField(max_lenght=200)
+    name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
@@ -16,12 +16,12 @@ class Categoty(models.Model):
 
 class Product(models.Model):
     
-    category = models.ForeignKey(Categoty,related_name='products',on_delete=CASCADE)
-    name = models.CharField(max_lenght=200)
+    category = models.ForeignKey(Categoty,related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank = True)
     description = models.TextField(blank=True)
-    price = models.IntegerField(max_digits=10, decimal_places = 2)
+    price = models.IntegerField(max_length=6)
     available = models.BooleanField()
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now = True)
