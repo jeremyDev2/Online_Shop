6a6d15d5b4ec1d803f0a7116ed8482939fe5bb06
Daniel <danil.malaev3@gmail.com>
2026-02-27 22:43:42 +0200

    fix: typos

--- ecommerce_shop/shop/views.py
+++ ecommerce_shop/shop/views.py
@@ -3,8 +3,8 @@ from .models import Category, Product
 
 def product_list(request, category_slug=None):
     category = None
-    categories = Category.object.all()
-    products = Product.object.filter(available=True)
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -13,5 +13,3 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
-
-
