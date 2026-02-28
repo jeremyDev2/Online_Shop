22150bef45d2f6b67297cdac8f300d3d0793a8ed
Daniel <danil.malaev3@gmail.com>
2026-02-28 14:47:34 +0200

    add: object with product form

--- ecommerce_shop/cart/forms.py
+++ ecommerce_shop/cart/forms.py
@@ -1,8 +1,9 @@
 from  django import forms
 
+#[(1, "1"), (2, "2"), ..., (20, "20")]
 PRODUCT_QUANTITY_CHOISES = [(i, str(i)) for i in range(1,21)]
 
-class CartAddProductionForm(forms.Form):
+class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOISES,coerce=int) #coerce - convert to int
     #False - new quantity added to existing one, True - existing quantity overrided with the given one
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
