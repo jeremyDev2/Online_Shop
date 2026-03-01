a49bd76bb05ebcc66b7c899de3f17d0cb4c4bdb9
Daniel <danil.malaev3@gmail.com>
2026-03-01 18:17:58 +0200

    add: cart_product_form in detail handler & types annotation

--- ecommerce_shop/shop/views.py
+++ ecommerce_shop/shop/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
+from cart.forms import CartAddProductForm
 from .models import Category, Product
+from django.shortcuts import render, get_object_or_404
 
-def product_list(request, category_slug=None):
+def product_list(request: HttpRequest, category_slug:str|None=None) -> HttpResponse:
     category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    categories:QuerySet[Category] = Category.objects.all()
+    products:QuerySet[Product] = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html', {'category' : category, 'products': products, 'categories':categories})
 
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    return render(request, 'shop/product/detail.html', {'product': product})
+def product_detail(request:HttpRequest, id, slug:str|None) -> HttpResponse:
+    product:Product= get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form:CartAddProductForm = CartAddProductForm()
+    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form':cart_product_form})
