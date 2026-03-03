47956471e9fcb5c2c9c488ed78cd028db2c3144c
Daniel <danil.malaev3@gmail.com>
2026-03-03 21:57:58 +0200

    add: order create handler

--- ecommerce_shop/orders/views.py
+++ ecommerce_shop/orders/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse
+from cart.cart import Cart
 from django.shortcuts import render
+from .forms import OrderCreateForm
+from .models import OrderItem
 
-# Create your views here.
+def order_create(request: HttpRequest) -> HttpResponse:
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order:OrderCreateForm = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item["product"],price=item["price"],quantity=item["quantity"],)
+            cart.clear()
+            return render(request, "orders/order/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+    return render(request,"orders/order/create.html", {"cart": cart, "form": form})
