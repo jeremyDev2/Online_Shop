a5b8f543bf38ab4c0864a164c2cf14cc2273f048
Danial <114513984+jeremyDev2@users.noreply.github.com>
2026-03-21 01:53:20 +0200

    Remove trash from settings.py

--- ecommerce_shop/ecommerce_shop/settings.py
+++ ecommerce_shop/ecommerce_shop/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c7tk2@7z4@1(kg12vf@p%a6qd6&@l=2#g(5_ai-%cqgxmx#p4c'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
