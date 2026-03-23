82479779df28590de95cc767b7d9ff39041cb1e8
Daniel <danil.malaev3@gmail.com>
2026-03-23 23:08:58 +0200

    add: order_pdf handler

--- ecommerce_shop/orders/views.py
+++ ecommerce_shop/orders/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .forms import OrderCreateForm
 from .models import OrderItem, Order
 from .tasks import order_created
+from django.contrib.staticfiles import finders
+from django.template.loader import render_to_string
+import weasyprint
 
 def order_create(request: HttpRequest) -> HttpResponse:
     cart = Cart(request)
@@ -34,3 +37,13 @@ def order_create(request: HttpRequest) -> HttpResponse:
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order':order})
+
+#only staff users can access this view
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order':order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename-order_{order_id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))])
+    return response
