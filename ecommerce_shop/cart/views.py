1cd1be1c8f8af19b1af15c150cf727c81fcc14b1
Daniel <danil.malaev3@gmail.com>
2026-03-27 23:35:17 +0200

    add: coupon apply form in cart_detail handler

--- ecommerce_shop/cart/views.py
+++ ecommerce_shop/cart/views.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from .cart import Cart
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404, redirect, render
+from coupons.forms import CouponApplyForm
 
 @require_POST
 def cart_add(request:HttpRequest, product_id:int) -> HttpResponse:
@@ -27,4 +28,5 @@ def cart_detail(request:HttpRequest) -> HttpResponse:
     for item in cart:
         #key for template
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override':True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
