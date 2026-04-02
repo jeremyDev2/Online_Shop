93faa25e01aa7c56527585a9763c4c19320e1e1f
Daniel <danil.malaev3@gmail.com>
2026-04-02 12:34:44 +0300

    add: test label for translation

--- ecommerce_shop/cart/forms.py
+++ ecommerce_shop/cart/forms.py
@@ -1,9 +1,10 @@
 from  django import forms
+from django.utils.translation import gettext_lazy as _
 
 #[(1, "1"), (2, "2"), ..., (20, "20")]
 PRODUCT_QUANTITY_CHOISES = [(i, str(i)) for i in range(1,21)]
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOISES,coerce=int) #coerce - convert to int
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOISES,coerce=int, label=_('Quantity')) #coerce - convert to int
     #False - new quantity added to existing one, True - existing quantity overrided with the given one
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
