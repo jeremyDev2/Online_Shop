d1f832f50bc63d9d067693722599b6f862303bda
Daniel <danil.malaev3@gmail.com>
2026-04-02 11:26:55 +0300

    update: cart detail handler

--- ecommerce_shop/cart/views.py
+++ ecommerce_shop/cart/views.py
@@ -5,6 +5,7 @@ from .cart import Cart
 from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404, redirect, render
 from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 @require_POST
 def cart_add(request:HttpRequest, product_id:int) -> HttpResponse:
@@ -29,4 +30,10 @@ def cart_detail(request:HttpRequest) -> HttpResponse:
         #key for template
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'], 'override':True})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    if (cart_products):
+        recommended_products = r.suggest_the_product(cart_products, max_result=4)
+    else:
+        recommended_products = []
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form, 'recommended_products': recommended_products})
