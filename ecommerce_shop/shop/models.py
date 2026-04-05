b93b75c5936c32b3665cbd6ea548fe37e46ec932
Daniel <danil.malaev3@gmail.com>
2026-04-05 23:09:20 +0300

    fix: now sorting in translations__name & Category/Product name/slug  indexe's

--- ecommerce_shop/shop/models.py
+++ ecommerce_shop/shop/models.py
@@ -3,44 +3,47 @@ from django.urls import reverse
 from django.utils import translation
 from parler.models import TranslatableModel, TranslatedFields
 
+
 class Category(TranslatableModel):
     translations = TranslatedFields(
-    name = models.CharField(max_length=200),
-    slug = models.SlugField(max_length=200, unique=True),)
+        name=models.CharField(max_length=200),
+        slug=models.SlugField(max_length=200, unique=True),
+    )
 
     class Meta:
-        ordering=["name"]
-        indexes=[models.Index(fields=["name"])]
+        ordering = ["translations__name"]
         verbose_name = "category"
         verbose_name_plural = "categories"
 
     def __str__(self) -> str:
-        return str(self.name)
-    
-    #the convention to retrieve the URL for a given object.
+        return str(self.safe_translation_getter("name", any_language=True))
+
+    # the convention to retrieve the URL for a given object.
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse("shop:product_list_by_category", args=[self.slug])
+
 
 class Product(TranslatableModel):
-    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
-    translation = TranslatedFields(name = models.CharField(max_length=200),
-                                   slug = models.SlugField(max_length=200, unique=True),
-                                   description = models.TextField(blank=True))
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank = True)
-    price = models.IntegerField(max_length=6)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    translations = TranslatedFields(
+        name=models.CharField(max_length=200),
+        slug=models.SlugField(max_length=200, unique=True),
+        description=models.TextField(blank=True),
+    )
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
+    price = models.IntegerField()
     available = models.BooleanField()
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now = True)
-
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['name']
-        indexes=[models.Index(fields=['id', 'slug']),
-                 models.Index(fields=['name']),
-                 models.Index(fields=['-created']),]
+        ordering = ["translations__name"]
+        indexes = [models.Index(fields=["-created"])]
 
     def __str__(self) -> str:
-        return str(self.name)
-    
+        return str(self.safe_translation_getter("name", any_language=True))
+
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse("shop:product_detail", args=[self.id, self.slug])
