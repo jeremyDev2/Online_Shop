bc1f2655b6df3c0c6494bfbeca3d1ba197aa9f59
Daniel <danil.malaev3@gmail.com>
2026-04-05 23:05:40 +0300

    fix: unsupported prepopulated_fields attribute in parler

--- ecommerce_shop/shop/admin.py
+++ ecommerce_shop/shop/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from .models import Category, Product
+from parler.admin import TranslatableAdmin
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ['name', 'slug']
     #specify fields where the value is automatically set using the value of other fields.
-    prepopulated_fields = {'slug' : ('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslatableAdmin):
     list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
     #set the fields that can be edited from the list display page of the administration site.
     list_editable  =['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
